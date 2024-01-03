f607a41af7dc186cbeb6f66654c9635ed6119e57
paul <423357+toothbrush@users.noreply.github.com>
2024-01-03 16:11:38 +1100

    omg, i can connect to id.atlassian.com!

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/toothbrush/confluence-dump
+
+go 1.21.5
+
+require (
+	github.com/magefile/mage v1.14.0 // indirect
+	github.com/virtomize/confluence-go-api v1.4.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	goconfluence "github.com/virtomize/confluence-go-api"
+)
+
+func main() {
+	token, err := exec.Command("pass", "confluence-api-token/paul.david@redbubble.com").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token_lines := strings.Split(strings.TrimSuffix(string(token), "\n"), "\n")
+
+	// initialize a new api instance
+	api, err := goconfluence.NewAPI("https://redbubble.atlassian.net/wiki/rest/api", "paul.david@redbubble.com", token_lines[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get current user information
+	currentUser, err := api.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", currentUser)
+}
