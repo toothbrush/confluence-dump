32813e5fb31348f5762fd7808707ac3e4eb42512
paul <423357+toothbrush@users.noreply.github.com>
2024-01-05 12:18:12 +1100

    cmd_config_which might be useful

---
+++ cmd/cmd_config_which.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2024 paul <paul@denknerd.org>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// whichCmd represents the which command
+var whichCmd = &cobra.Command{
+	Use:   "which",
+	Short: "Tell me the resolved config path",
+	Long: `
+Output the filename that's being used to store your config.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Config path: %s\n", ConfigActual)
+	},
+}
+
+func init() {
+	configCmd.AddCommand(whichCmd)
+}
