5f2ce39365d78557c77ec86f9f5f2c7af38c592b
paul <423357+toothbrush@users.noreply.github.com>
2024-01-05 12:17:43 +1100

    cmd_config just a placeholder

--- cmd/cmd_config.go
+++ cmd/cmd_config.go
@@ -4,8 +4,6 @@ Copyright © 2024 paul <paul@denknerd.org>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,14 +12,9 @@ var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Commands to work with the app config",
 	Long: `
-
 Commands in this namespace are to help you configure the app.  Find out what the current config is,
 or learn where it's being read from.
-
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("config called")
-	},
 }
 
 func init() {
