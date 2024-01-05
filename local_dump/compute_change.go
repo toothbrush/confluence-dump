55e63152f82e0120b0a2e7ea1ed6432fd8c9e310
paul <423357+toothbrush@users.noreply.github.com>
2024-01-05 17:06:57 +1100

    less debug spam

--- local_dump/compute_change.go
+++ local_dump/compute_change.go
@@ -2,31 +2,25 @@ package local_dump
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/toothbrush/confluence-dump/data"
 )
 
 func LocalPageIsStale(content data.ConfluenceContent, remote_cache data.RemoteContentCache, local_files data.LocalMarkdownCache) (bool, error) {
 	id := data.ContentID(content.Content.ID)
-	fmt.Printf("LocalPageIsStale for id = %s\n", id)
 
 	if remote, ok := remote_cache[id]; ok {
-		fmt.Printf("querying local_files for %s\n", id)
 		if our_item, ok := local_files[id]; ok {
-			fmt.Printf("our_item = %s, %d, %s\n", our_item.ID, our_item.Version, our_item.RelativePath)
 			// ok, we _are_ aware of it.  how about the version?
 			if our_item.Version == remote.Version {
 				// oh, we know about it, and it's the same version! nothing to do here.
 				return false, nil
 			} else {
 				// it's a different version.  redownload.
-				fmt.Fprintf(os.Stderr, "present: staleness debugging: %d, %d, %s\n", our_item.Version, remote.Version, our_item.RelativePath)
 				return true, nil
 			}
 		} else {
 			// we don't have the remote item at all -- add it to the download list
-			fmt.Fprintf(os.Stderr, "absent: staleness debugging: %d, %d, %s\n", our_item.Version, remote.Version, our_item.RelativePath)
 			return true, nil
 		}
 	} else {
