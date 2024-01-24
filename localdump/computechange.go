921d6ee4208384945811938a0ce63500445c9ed0
paul <423357+toothbrush@users.noreply.github.com>
2024-01-24 23:37:12 +1100

    computechange: Gotta be smart about modified ancestors.
    Unfortunately, if we don't consider a version change in an ancestor, we
    might miss a rename or file move that needs to happen.  I'm sure we
    could do this more efficiently by precomputing rather than doing it from
    the leaves of the tree up for each page, but oh well.

--- localdump/computechange.go
+++ localdump/computechange.go
@@ -21,10 +21,15 @@ func (downloader *SpacesDownloader) LocalVersionIsRecent(pageID ContentID) (Loca
 	remoteAncestry := remote.AncestorIDs
 	localAncestry := ourItem.AncestorIDs
 
+	ancestryEqual, err := downloader.ancestryEqual(remoteAncestry, localAncestry)
+	if err != nil {
+		return LocalMarkdown{}, false, fmt.Errorf("localdump: error comparing ancestry: %w", err)
+	}
+
 	// ok, we _are_ aware of it.  how about the version?
 	if remote.Page.Version != nil &&
 		remote.Page.Version.Number == ourItem.Version &&
-		ancestryEqual(remoteAncestry, localAncestry) {
+		ancestryEqual {
 		// oh, we know about it, and it's the same version & ancestry! nothing to do here.
 		return ourItem, true, nil
 	} else {
@@ -33,17 +38,44 @@ func (downloader *SpacesDownloader) LocalVersionIsRecent(pageID ContentID) (Loca
 	}
 }
 
-func ancestryEqual(ancestry1 []ContentID, ancestry2 []ContentID) bool {
+func (downloader *SpacesDownloader) ancestryEqual(ancestry1 []ContentID, ancestry2 []ContentID) (bool, error) {
 	if len(ancestry1) != len(ancestry2) {
-		return false
+		return false, nil
 	}
 
-	// now we know their lengths are equal.
+	// now we know their lengths are equal, so we can safely iterate on the one index.
 	for i := range ancestry1 {
-		if ancestry1[i] != ancestry2[i] {
-			return false
+		// first, we make sure that the ids of the ancestors are equal:
+		ancestor1 := ancestry1[i]
+		ancestor2 := ancestry2[i]
+		if ancestor1 != ancestor2 {
+			return false, nil
+		}
+
+		// however, we should refresh sub-pages if an ancestor was updated.  otherwise we'll miss
+		// the case where a->b->c and b is renamed, in which case the folders on our local copy
+		// should be renamed too.
+		//
+		// in time, we can probably make this more efficient by precomputing staleness in a DFS
+		// way.... oh well.
+		remoteAncestorMetadata, ok := downloader.remotePageMetadata[ancestor1]
+		if !ok {
+			return false, fmt.Errorf("localdump: could not look up remote ancestor metadata: %s", ancestor1)
+		}
+
+		localAncestorMetadata, ok := downloader.localMarkdownCache[ancestor1]
+		if !ok {
+			return false, fmt.Errorf("localdump: could not look up local ancestor cached metadata: %s", ancestor1)
+		}
+
+		if remoteAncestorMetadata.Page.Version == nil {
+			return false, fmt.Errorf("localdump: unexpected nil .Version for ancestor: %+v", remoteAncestorMetadata)
+		}
+
+		if remoteAncestorMetadata.Page.Version.Number != localAncestorMetadata.Version {
+			return false, nil
 		}
 	}
 
-	return true
+	return true, nil
 }
